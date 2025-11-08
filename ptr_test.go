83969b26062fa63e353a5a2605918df2ff517301
raashed <mohammedraashed10@gmail.com>
2025-11-08 06:47:06 +0530

    test: add benchmarks for pointer and value parameter functions - Implemented BenchmarkPBP for measuring performance of fooPBP with pointer parameter. - Implemented BenchmarkPBV for measuring performance of fooPBV with value parameter. - Added tests to ptr_test.go to ensure performance profiling. - Utilized the testing package to leverage Go's benchmarking utilities.

---
+++ ptr_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func BenchmarkPBP(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fooPBP(&obj)
+	}
+}
+
+func BenchmarkPBV(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		fooPBV(obj)
+	}
+}
