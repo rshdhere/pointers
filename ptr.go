f88f922251c02a903163972ac634f94ceb51eb92
raashed <mohammedraashed10@gmail.com>
2025-11-08 06:37:25 +0530

    chore: add .gitignore and initial implementation of BigStruct in ptr.go - Create a .gitignore file to exclude dependency directories and environment files. - Implement BigStruct with a buffer for memory allocation in ptr.go. - Define two functions for passing BigStruct by value and by pointer. - Ensure that `foo` demonstrates usage of both passing methods.

---
+++ ptr.go
@@ -0,0 +1,16 @@
+package main
+
+type BigStruct struct {
+	buf [1 << 16]byte
+}
+
+var obj BigStruct = BigStruct{}
+
+func fooPBV(obj BigStruct) {}
+
+func fooPBP(obj *BigStruct) {}
+
+func foo() {
+	fooPBV(obj)
+	fooPBP(&obj)
+}
